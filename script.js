9cf315b6ddcabac942ac29734817a63f67f43242
Asad Sultan <76148295+Asad-Sultan@users.noreply.github.com>
2023-03-27 21:29:17 +0500

    idk
    Designed the hover menu that changes max marks. Also did display flex instead of inline block for results

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const allSubjects = ["Applied Physics", "Programming Fundamentals", "Introduction to Logics", "Introduction to Communication and Information", "English Composition and Comprehension"];
-const inputTypeList = ["Assignments", "Quizes", "Mid", "Final", "Project", "Lab"];
+const allSubjects = [
+  "Applied Physics",
+  "Programming Fundamentals",
+  "Introduction to Logics",
+  "Introduction to Communication and Information",
+  "English Composition and Comprehension",
+];
+const inputTypeList = [
+  "Assignments",
+  "Quizes",
+  "Mid",
+  "Final",
+  "Project",
+  "Lab",
+];
 var selectedSubjects = [];
 // var labels = [];
 
@@ -13,46 +26,28 @@ class Subject {
 
 var subObj = [];
 
-subObj.push(new Subject(
-  "physics",
-  [40, 40, 30, 40, 20],
-  [10, 10, 20, 40, 20]
-));
+subObj.push(new Subject("physics", [40, 40, 30, 40, 20], [10, 10, 20, 40, 20]));
 
-subObj.push(new Subject(
-  "pf",
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0]
-));
+subObj.push(new Subject("pf", [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]));
 
-subObj.push(new Subject(
-  "ict",
-  [0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0]
-));
+subObj.push(new Subject("ict", [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]));
 
-subObj.push(new Subject(
-  "eng",
-  [0, 0, 0, 0],
-  [0, 0, 0, 0]
-));
+subObj.push(new Subject("eng", [0, 0, 0, 0], [0, 0, 0, 0]));
 
-subObj.push(new Subject(
-  "logics",
-  [0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0]
-));
+subObj.push(new Subject("logics", [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]));
 
 var changingSubject = subObj[0];
 var changingIndex = 0;
 
 function updateMax(element) {
   if (!(document.getElementById("change-txt").value === "")) {
-    changingSubject.max[changingIndex] = Number(document.getElementById("change-txt").value);
+    changingSubject.max[changingIndex] = Number(
+      document.getElementById("change-txt").value
+    );
     element.innerHTML = " / " + document.getElementById("change-txt").value;
   }
 
-  document.querySelectorAll(".hoveringPrompt").forEach(element => {
+  document.querySelectorAll(".hoveringPrompt").forEach((element) => {
     element.remove();
   });
 }
@@ -71,39 +66,39 @@ function addListeners(labels) {
 
       b.innerHTML = "Change?";
       b.id = "change-btn";
-      b.className = "change-btn";
+      b.className = "change-btn btn";
       div.append(t);
       div.append(b);
 
       div.className = "hoveringPrompt";
-      div.style.left = (ev.clientX).toString() + "px";
-      div.style.top = (ev.clientY).toString() + "px";
+      div.style.left = ev.clientX.toString() + "px";
+      div.style.top = ev.clientY.toString() + "px";
 
       changingIndex = labels.indexOf(element);
 
-      document.querySelectorAll(".hoveringPrompt").forEach(el => {
+      document.querySelectorAll(".hoveringPrompt").forEach((el) => {
         el.remove();
-      })
+      });
 
       document.body.appendChild(div);
 
       document.onkeydown = function (evt) {
         evt = evt || window.event;
         if ("key" in evt) {
-          if(evt.key === "Enter") {
+          if (evt.key === "Enter") {
             updateMax(element);
-          } else if(evt.key === "Escape") {
-            document.querySelectorAll(".hoveringPrompt").forEach(element => {
+          } else if (evt.key === "Escape") {
+            document.querySelectorAll(".hoveringPrompt").forEach((element) => {
               element.remove();
             });
           }
         }
-      }
+      };
 
       document.getElementById("change-btn").onclick = function () {
         updateMax(element);
       };
-    })
+    });
   });
 }
 
@@ -147,21 +142,21 @@ function calculateTotal(subject) {
     let marks = document.getElementById(id).value;
     if (marks < 0) marks = 0;
     if (marks > subject.max[i]) marks = subject.max[i];
-    totalMarks += marks / subject.max[i] * subject.weight[i];
+    totalMarks += (marks / subject.max[i]) * subject.weight[i];
   }
   return totalMarks.toFixed(2);
 }
 
 function getGrade(marks) {
   if (marks >= 85) return 4;
-  if (marks >= 80) return 4 - (1 / 3);
-  if (marks >= 75) return 4 - (2 / 3);
+  if (marks >= 80) return 4 - 1 / 3;
+  if (marks >= 75) return 4 - 2 / 3;
   if (marks >= 71) return 3;
-  if (marks >= 68) return 3 - (1 / 3);
-  if (marks >= 64) return 3 - (2 / 3);
+  if (marks >= 68) return 3 - 1 / 3;
+  if (marks >= 64) return 3 - 2 / 3;
   if (marks >= 61) return 2;
-  if (marks >= 58) return 2 - (1 / 3);
-  if (marks >= 54) return 2 - (2 / 3);
+  if (marks >= 58) return 2 - 1 / 3;
+  if (marks >= 54) return 2 - 2 / 3;
   if (marks >= 50) return 1;
   return 0;
 }
@@ -169,7 +164,7 @@ function getGrade(marks) {
 createInputs(subObj[0]);
 
 document.getElementById("sub").onclick = () => {
-  document.getElementById("cardDiv").innerHTML = '';
+  document.getElementById("cardDiv").innerHTML = "";
 
   for (let i = 0; i < subObj.length; i++) {
     if (document.getElementById("sub").value === subObj[i].name) {
@@ -191,7 +186,7 @@ document.getElementById("calculate-btn").onclick = () => {
   document.getElementById("showMarks").innerHTML = marks;
   document.getElementById("showGrade").innerHTML = getGrade(marks);
 
-  Array.from(document.getElementsByClassName("results")).forEach(element => {
-    element.style.display = "inline-block";
+  Array.from(document.getElementsByClassName("results")).forEach((element) => {
+    element.style.display = "flex";
   });
 };
