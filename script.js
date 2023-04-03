2ab2c8f56e18fba765d5e85153d7bad0b284fb8f
Asad Sultan <76148295+Asad-Sultan@users.noreply.github.com>
2023-04-03 07:05:16 +0500

    Fixed tooltip overflow bug

--- script.js
+++ script.js
@@ -497,7 +497,6 @@ document.getElementById("add-btn").onclick = () => {
       sleep(1).then(() => {
         element.style.maxHeight = i + "px";
       });
-      element.style.overflow = "visible";
     }
     // element.className = "subject-wrapper-new";
   });
